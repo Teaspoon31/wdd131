5b0bb8eaa2d988d76d431f92858f596a376086e4
teaspoon31 <jboateng4@byupathway.edu>
2026-05-23 20:45:06 +0000

    place

--- scripts/place.js
+++ scripts/place.js
@@ -1,48 +1,29 @@
-// CURRENT YEAR
-
 document.getElementById("year").textContent =
     new Date().getFullYear();
-
-
-// LAST MODIFIED DATE
 
 document.getElementById("lastModified").textContent =
     `Last Modified: ${document.lastModified}`;
 
-
-
-// STATIC WEATHER VALUES
-
 const temperature = 28;
 const windSpeed = 12;
-
-
-
-// WIND CHILL FUNCTION
 
 function calculateWindChill(temp, speed) {
 
     return (
         13.12 +
         0.6215 * temp -
-        11.37 * speed ** 0.16 +
-        0.3965 * temp * speed ** 0.16
+        11.37 * Math.pow(speed, 0.16) +
+        0.3965 * temp * Math.pow(speed, 0.16)
     ).toFixed(1);
 }
 
-
-
-// DISPLAY WIND CHILL
-
 let windChill = "N/A";
-
 
 if (temperature <= 10 && windSpeed > 4.8) {
 
     windChill =
         `${calculateWindChill(temperature, windSpeed)} °C`;
 }
-
 
 document.getElementById("windchill").textContent =
     windChill;
